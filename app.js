3f673f3cd6c69df8edd0f5ddce1b6c3e379ae5d0
Charles Frederick <viyperstrike@gmail.com>
2023-12-06 15:29:09 -0500

    Why does homeCard do that

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    console.log("Connection to index!");
-});
 app.get('/home', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home'));
 app.get('*', (req, res) => res.send('404 Page Not Found'));
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server open on ${port}`);
