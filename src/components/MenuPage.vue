cbc6615a886a88f3a23e5a14f67413aac11c2e40
Zander <zanderinderstrodt@gmail.com>
2024-02-26 13:30:43 -0500

    +I added comments so you can understand this mess +added the word setup

--- src/components/MenuPage.vue
+++ src/components/MenuPage.vue
@@ -1,23 +1,29 @@
 <script setup>
 import menu from '../assets/json/menu.json';
 import anime from 'animejs';
-      let categoryOriginal;
+    //Store the original category details for reverse animation
+    let categoryOriginal;
 
     function openCategory(category){
+      //Get the clicked category, if reverse is needed, and the title of the page 
       category = document.getElementById(category);
       let reverse = category.getAttribute('reverse');
       let pageTitle = document.getElementsByClassName('title')[0];
+      //Store original category if this is the first animation
       if(reverse === 'false'){
         categoryOriginal = category.getBoundingClientRect();
       }
+      //This is a hack to get the location of where the text starts and ends on the page title so i can move stuff properly
       let range = document.createRange();
       range.setStart(pageTitle.firstChild, 0);
       range.setEnd(pageTitle.firstChild, pageTitle.textContent.length);
+      //Get all the categories(this should work with more categories if we want!)
       let c = document.getElementsByClassName('category');
       c = [...c];
       let categoryLoc;
       let larger = [];
       let smaller = [];
+      //Sort the categories into smaller and larger than the clicked category with smaller being to the left and larger to the right
       for (let i = 0; i < c.length; i++) {
         if (c[i] == category) {
           categoryLoc = i;
@@ -31,8 +37,9 @@ import anime from 'animejs';
           larger.push(c[i]);
         }
       }
-
+      //Change the reverse value to the opposite so the next time the animation is called it will reverse, this will work always because you shouldnt be able to click any other categories after the first click
       category.setAttribute('reverse', reverse === 'false' ? 'true' : 'false');
+      //Fuck anime.js
       anime({
           targets: [smaller],
           translateX: reverse === 'false' ? '-100vw' : 0,
@@ -56,6 +63,7 @@ import anime from 'animejs';
         duration: 1000,
         easing: 'spring(1, 80, 14.5, 0)',
       });
+      //hide the title of the page with a " " which is just a hidden character cause for some reason it will jsut remvoe the element all together if you use a space or \n
       pageTitle.innerHTML = reverse === 'false' ? " " : pageTitle.getAttribute('text');
     }
 </script>
