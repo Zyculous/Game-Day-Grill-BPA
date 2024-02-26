4f75906da694eccdbba16bc2b970543d76afd9d9
Zander <zanderinderstrodt@gmail.com>
2024-02-26 10:57:52 -0500

    +ANIMATION (except apps)

--- src/components/MenuPage.vue
+++ src/components/MenuPage.vue
@@ -5,45 +5,22 @@ export default {
   data() {
     return {
       menu,
-      collapsedStates: {},
+      categoryOriginal: null
     };
   },
   mounted() {
-    this.initializeCollapsedStates();
   },
   methods: {
-    initializeCollapsedStates() {
-      Object.keys(this.menu).forEach(category => {
-        this.collapsedStates[category] = false;
-      });
-    },
-    hideItems(category) {
-      // Toggle the category state
-      
-      this.collapsedStates[category] = !this.collapsedStates[category];
-
-      // Delay the hiding of items to allow for animation
-      if (this.collapsedStates[category]) {
-        const items = document.querySelectorAll('.' + category);
-        items.forEach((item, index) => {
-          setTimeout(() => {
-            item.classList.add('hidden');
-            console.log(item);
-          }, index * 20);
-        });
-      }else{
-        const items = document.querySelectorAll('.' + category);
-        items.forEach((item, index) => {
-          setTimeout(() => {
-            item.classList.remove('hidden');
-            console.log(item);
-          }, index * 20);
-        });
-      }
-    },
     openCategory(category){
       category = document.getElementById(category);
       let reverse = category.getAttribute('reverse');
+      let pageTitle = document.getElementsByClassName('title')[0];
+      if(reverse === 'false'){
+        this.categoryOriginal = category.getBoundingClientRect();
+      }
+      let range = document.createRange();
+      range.setStart(pageTitle.firstChild, 0);
+      range.setEnd(pageTitle.firstChild, pageTitle.textContent.length);
       let c = document.getElementsByClassName('category');
       c = [...c];
       let categoryLoc;
@@ -62,65 +39,34 @@ export default {
           larger.push(c[i]);
         }
       }
-      let title = document.getElementsByClassName('title')[0];
-      //console.log(title);
-      //c.splice(categoryLoc, 1);
-      console.log("Catergory: " + category.getAttribute('id'));
-      console.log("Loc: " + categoryLoc);
-      console.log("Larger: " + larger);
-      console.log("Smaller: " + smaller);
-      console.log("Reverse: " + reverse);
-      console.log("CategoryX: " + category.getBoundingClientRect().x);
-      console.log("TitleX: " + title.getBoundingClientRect().x);
 
-      if(reverse == 'true'){
-        anime({
-          targets: [c, category],
-          translateX: '0',
-          translateY: '0',
-          duration: 1000,
-          delay: anime.stagger(100),
-          easing: 'spring(1, 80, 14.5, 0)',
-          width: '100%',
-          height: '100%',
-          complete: function(){
-            title.innerHTML = title.getAttribute('text');
-          }
-        });
-        category.setAttribute('reverse', 'false');
-      }else{
-        anime({
-          targets: [larger],
-          translateX: '150vw',
-          duration: 1000,
-          delay: anime.stagger(100, {direction: 'reverse'}),
-          easing: 'spring(1, 80, 14.5, 0)'
-        });
-        anime({
+      category.setAttribute('reverse', reverse === 'false' ? 'true' : 'false');
+      anime({
           targets: [smaller],
-          translateX: '-150vw',
-          duration: 1000,
+          translateX: reverse === 'false' ? '-100vw' : 0,
           delay: anime.stagger(100),
-          easing: 'spring(1, 80, 14.5, 0)'
-        });
-        anime({
-          targets: [category],
-          translateX: function(category){
-            return ((title.getBoundingClientRect().right + title.getBoundingClientRect().left)/2) - ((category.getBoundingClientRect().right + category.getBoundingClientRect().left)/2);
-          },
-          translateY: function(){
-            return ((title.getBoundingClientRect().bottom + title.getBoundingClientRect().top)/2) - ((category.getBoundingClientRect().bottom + category.getBoundingClientRect().top)/2);
-          },
-          width: '100vh',
-          height: '35%',
           duration: 1000,
           easing: 'spring(1, 80, 14.5, 0)',
-          begin: function(){
-            title.innerHTML = " ";
-          }
-        });
-        category.setAttribute('reverse', 'true');
-      }
+      })
+      anime({
+          targets: [larger],
+          translateX: reverse === 'false' ? '100vw' : 0,
+          delay: anime.stagger(100, {direction: 'reverse'}),
+          duration: 1000,
+          easing: 'spring(1, 80, 14.5, 0)',
+      })
+      anime({
+        targets: [category],
+        translateY: reverse === 'false' ? -(category.getBoundingClientRect().y - pageTitle.getBoundingClientRect().y) : 0,
+        translateX: reverse === 'false' ? (range.getBoundingClientRect().x - category.getBoundingClientRect().x) : 0,
+        width: reverse === 'false' ? '100vw' : `${this.categoryOriginal.width}px`,
+        height: reverse === 'false' ? '10%' : `${this.categoryOriginal.height}px`,
+        duration: 1000,
+        easing: 'spring(1, 80, 14.5, 0)',
+      });
+      pageTitle.innerHTML = reverse === 'false' ? " " : pageTitle.getAttribute('text');
+
+
     }
   }
 };
@@ -128,7 +74,7 @@ export default {
 
 <template>
     <div class="menu-page">
-        <h1 class="green center title" text= "Menu Page">Menu Page</h1>
+        <h1 class="center title" text= "Menu&nbsp;Page">Menu&nbsp;Page</h1>
         <div class="menu">
             <div class="category" :id="category" reverse='false' @click="openCategory(category)" v-for="(items, category) in menu" :key="category">
                 <h2 class="green center point">{{ category }}</h2>
@@ -154,7 +100,9 @@ export default {
     align-items: center;
     justify-content: center;
 }
-
+.title{
+  color: #85ae84;
+}
 .category{
     display: flex;
     flex-direction: column;
