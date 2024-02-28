5a55af7f4a259297551ab34b17e1ba899edd0078
Zander <zanderinderstrodt@gmail.com>
2024-02-28 15:19:54 -0500

    +dumb bubbles

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -10,8 +10,32 @@
                 {value: () => `rgb(${anime.random(128, 255)}, ${anime.random(1, 74)}, ${anime.random(100, 216)})`}, //Pink color scale
                 {value: () => `rgb(${anime.random(255, 181)}, ${anime.random(243, 169)}, ${anime.random(74, 4)})`}, //Yellow color scale
                 {value: () => `rgb(${anime.random(83, 5)}, ${anime.random(247, 151)}, ${anime.random(252, 156)})`} //Light blue color scale
-            ]
-        for(let i=0; i<70; i++){
+            ];
+            let randomKeyframe = [
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)},
+                {value: () => anime.random(-800, 800)}
+            ];
+            let randomKeyframeSmall = [
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)},
+                {value: () => anime.random(-500, 500)}
+            ];
+        for(let i=0; i<50; i++){
             let block;
             block = document.createElement("div");
             block.classList.add("bubble");
@@ -22,8 +46,8 @@
         const animation = () => {
             anime({
                 targets: [...bubbles],
-                translateX: () => `${anime.random(-1000, 1000)}`,
-                translateY: () => `${anime.random(-800, 800)}`,
+                translateX: randomKeyframe[anime.random(0, 9)],
+                translateY: randomKeyframeSmall[anime.random(0, 9)],
                 scale: () => anime.random(1, 3),
                 rotate: () => anime.random(-360, 360),
                 background : backgroundColors[anime.random(0, 6)],
