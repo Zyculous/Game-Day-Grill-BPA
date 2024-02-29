85a8439b1ab35f597451731ce62f88d3bc03eb6a
Zander <zanderinderstrodt@gmail.com>
2024-02-29 16:23:58 -0500

    +animation for page loading in

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import AboutPage from './components/AboutPage.vue'
 import ReservationsPage from './components/ReservationsPage.vue'
 import TestPage from './components/TestPage.vue'
 
-import { ref, computed } from 'vue'
+import { ref, computed, nextTick } from 'vue'
+import anime from 'animejs'
 
 const routes = {
   '/home': HomePage,
@@ -19,12 +20,58 @@ const routes = {
 const currentPath = ref(window.location.hash)
 console.log(currentPath.value)
 window.addEventListener('hashchange', () => {
+  loadPage();
   currentPath.value = window.location.hash
 })
 
 const currentView = computed(() => {
   return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
+function loadPage() {
+  nextTick(() => {
+    console.log("Loading page")
+    anime({
+      targets: '.page',
+      opacity: [0, 1],
+      translateY: [50, 0],
+      duration: 1000,
+      easing: 'easeInOutCubic'
+    });
+
+    let backgroundColors = [
+      {value: () => `rgb(${anime.random(50, 14)}, ${anime.random(168, 184)}, ${anime.random(70, 98)})`}, //Purple color scale
+      {value: () => `rgb(${anime.random(200, 145)}, ${anime.random(30, 10)}, ${anime.random(20, 4)})`}, //Red color scale
+      {value: () => `rgb(${anime.random(135, 68)}, ${anime.random(60, 2)}, ${anime.random(230, 156)})`}, //Cyan color scale
+      {value: () => `rgb(${anime.random(81, 29)}, ${anime.random(217, 128)}, ${anime.random(43, 1)})`}, //Green color scale
+      {value: () =>`rgb(${anime.random(128, 255)}, ${anime.random(1, 74)}, ${anime.random(100, 216)})`}, //Pink color scale
+      {value: () =>`rgb(${anime.random(255, 181)}, ${anime.random(243, 169)}, ${anime.random(74, 4)})`}, //Yellow color scale
+      {value: () =>`rgb(${anime.random(83, 5)}, ${anime.random(247, 151)}, ${anime.random(252, 156)})`} //Light blue color scale
+    ];
+    for (let i = 0; i < 1000; i++) {
+      let block
+      block = document.createElement('div')
+      block.classList.add('square')
+      block.style.display = 'flex'
+      block.style.top = `${anime.random(0, 100)}%`
+      block.style.left = `${anime.random(0, 100)}%`
+      block.id = `block${i}`
+      document.getElementById('page').appendChild(block);
+
+      anime({
+        targets: block,
+        backgroundColor: backgroundColors,
+        translateY: anime.random(-100, 100),
+        translateX: anime.random(-100, 100),
+        duration: anime.random(100, 2000),
+        easing: 'easeInOutCubic',
+        complete: function() {
+          document.getElementById('page').removeChild(document.getElementById(`block${i}`));
+        }
+      });
+      
+    }
+  });
+}
 </script>
 
 <template>
@@ -35,7 +82,7 @@ const currentView = computed(() => {
       <a class="link" href="#/reservations">Reservations</a> <p>|</p>
       <a class="link" href="#/about">About</a> <p>|</p>
     </div>
-    <div class="page">
+    <div class="page" id="page">
       <component :is="currentView" />
     </div>
   </div>
--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,65 +1,71 @@
-<script>
-    import anime from 'animejs';
-    export default {
-        mounted(){
-            let backgroundColors = [
-                {value: () => `rgb(${anime.random(50, 14)}, ${anime.random(168, 184)}, ${anime.random(70, 98)})`}, //Purple color scale
-                {value: () => `rgb(${anime.random(200, 145)}, ${anime.random(30, 10)}, ${anime.random(20, 4)})`}, //Red color scale
-                {value: () => `rgb(${anime.random(135, 68)}, ${anime.random(60, 2)}, ${anime.random(230, 156)})`}, //Cyan color scale
-                {value: () => `rgb(${anime.random(81, 29)}, ${anime.random(217, 128)}, ${anime.random(43, 1)})`}, //Green color scale
-                {value: () => `rgb(${anime.random(128, 255)}, ${anime.random(1, 74)}, ${anime.random(100, 216)})`}, //Pink color scale
-                {value: () => `rgb(${anime.random(255, 181)}, ${anime.random(243, 169)}, ${anime.random(74, 4)})`}, //Yellow color scale
-                {value: () => `rgb(${anime.random(83, 5)}, ${anime.random(247, 151)}, ${anime.random(252, 156)})`} //Light blue color scale
-            ];
-            let randomKeyframe = [
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)},
-                {value: () => anime.random(-800, 800)}
-            ];
-            let randomKeyframeSmall = [
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)},
-                {value: () => anime.random(-500, 500)}
-            ];
-        for(let i=0; i<50; i++){
-            let block;
-            block = document.createElement("div");
-            block.classList.add("bubble");
-            block.style.display = "flex";
-            document.getElementById('background').appendChild(block);
+<script setup>
+import anime from 'animejs'
+import { nextTick } from 'vue'
+nextTick(() => {
+  let backgroundColors = [
+    {value: () => `rgb(${anime.random(50, 14)}, ${anime.random(168, 184)}, ${anime.random(70, 98)})`}, //Purple color scale
+    {value: () => `rgb(${anime.random(200, 145)}, ${anime.random(30, 10)}, ${anime.random(20, 4)})`}, //Red color scale
+    {value: () => `rgb(${anime.random(135, 68)}, ${anime.random(60, 2)}, ${anime.random(230, 156)})`}, //Cyan color scale
+    {value: () => `rgb(${anime.random(81, 29)}, ${anime.random(217, 128)}, ${anime.random(43, 1)})`}, //Green color scale
+    {value: () =>`rgb(${anime.random(128, 255)}, ${anime.random(1, 74)}, ${anime.random(100, 216)})`}, //Pink color scale
+    {value: () =>`rgb(${anime.random(255, 181)}, ${anime.random(243, 169)}, ${anime.random(74, 4)})`}, //Yellow color scale
+    {value: () =>`rgb(${anime.random(83, 5)}, ${anime.random(247, 151)}, ${anime.random(252, 156)})`} //Light blue color scale
+  ]
+  for (let i = 0; i < 250; i++) {
+    let block
+    block = document.createElement('div')
+    block.classList.add('square')
+    block.style.display = 'flex'
+    block.style.top = `${anime.random(0, 100)}%`
+    block.style.left = `${anime.random(0, 100)}%`
+
+    document.getElementById('background').appendChild(block)
+  }
+
+  async function squareAnimation() {
+    setInterval(() => {
+      // Run this code every second
+      let squares = document.querySelectorAll('.square')
+      for (let square of squares) {
+        let oldX = parseInt(square.style.transform.split('(')[1]);
+        let newX = anime.random(0, 100);
+        //console.log("Old: " + oldX);
+        //console.log("New: " + newX);
+        square.style.transform = `translateX(${newX}px) translateY(${anime.random(0, 100)}px)`
+        /*
+        let oldTop = parseInt(square.style.top)
+        let oldLeft = parseInt(square.style.left)
+        let newTop = anime.random(0, 500)
+        let newLeft = anime.random(0, 500)
+
+        if (oldTop < newTop) {
+          for (let i = oldTop; i < newTop; i+=.1) {
+            square.style.top = `${i}px`
+          }
+        } else {
+          for (let i = oldTop; i > newTop; i-=.1) {
+            square.style.top = `${i}px`
+          }
         }
-        let bubbles = document.querySelectorAll('.bubble');
-        const animation = () => {
-            anime({
-                targets: [...bubbles],
-                translateX: randomKeyframe[anime.random(0, 9)],
-                translateY: randomKeyframeSmall[anime.random(0, 9)],
-                scale: () => anime.random(1, 3),
-                rotate: () => anime.random(-360, 360),
-                background : backgroundColors[anime.random(0, 6)],
-                easing: 'linear',
-                duration: anime.random(2000, 3000),
-                delay: anime.stagger(25),
-                complete: animation,
-            });
+        if (oldLeft < newLeft) {
+          for (let i = oldLeft; i < newLeft; i+=.1) {
+            square.style.left = `${i}px`
+          }
+        } else {
+          for (let i = oldLeft; i > newLeft; i-=.1) {
+            square.style.left = `${i}px`
+          }
         }
-        animation();
-    }
-}
+        // Apply the top and left values to the square
+        //square.style.top = `${anime.random(0, 100)}%`;
+        //square.style.left = `${anime.random(0, 100)}%`;*/
+        square.style.backgroundColor = backgroundColors[anime.random(0, 6)].value()
+      }
+    }, 300)
+  }
+  squareAnimation()
+  //bubbleAnimation();
+})
 </script>
 <template>
   <div class="test-page">
@@ -74,6 +80,11 @@
   width: 10px;
   height: 10px;
   border-radius: 50%;
+}
+.square {
+  position: absolute;
+  width: 10px;
+  height: 10px;
 }
 </style>
 
